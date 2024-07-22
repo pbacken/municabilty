198bac000fd414e516f54545e83a8d4e53c786c0
Pat Backen <pat.backen@gmail.com>
2024-07-22 15:02:14 -0500

    email templates, password management

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
+import jwt
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
+from app import db, login, app
 from flask_login import UserMixin
 from hashlib import md5
 from datetime import datetime, timezone
+from time import time
 from typing import Optional
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -28,6 +30,20 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except Exception as e:
+            return
+        return db.session.get(User, id)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
